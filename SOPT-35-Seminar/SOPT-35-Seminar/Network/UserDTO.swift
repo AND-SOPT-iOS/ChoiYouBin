c7f99490d1444927fa8a711166cbe033e973fbd2
최유빈 <cub8533@naver.com>
2024-11-08 15:40:52 +0900

    week/4 - [Feat] Environment, NetworkError, UserDTO, UserService 파일 작성

---
+++ SOPT-35-Seminar/SOPT-35-Seminar/Network/UserDTO.swift
@@ -0,0 +1,14 @@
+//
+//  UserDTO.swift
+//  SOPT-35-Seminar
+//
+//  Created by 최유빈 on 11/8/24.
+//
+
+import Foundation
+
+struct RegisterRequest: Codable {
+    let username: String
+    let password: String
+    let hobby: String
+}

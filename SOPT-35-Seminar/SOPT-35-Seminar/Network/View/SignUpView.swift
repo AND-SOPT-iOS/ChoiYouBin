a9eebf85abd7a5d3aa471e6f5d6a0a395e1e7917
ChoiYouBin <cub8533@naver.com>
2024-11-11 01:59:30 +0900

    week/4 - [Refactor] 컴포넌트 그룹화

---
+++ SOPT-35-Seminar/SOPT-35-Seminar/Network/View/SignUpView.swift
@@ -0,0 +1,20 @@
+//
+//  SignUpView.swift
+//  SOPT-35-Seminar
+//
+//  Created by 최유빈 on 11/11/24.
+//
+
+import UIKit
+
+class SignUpView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}

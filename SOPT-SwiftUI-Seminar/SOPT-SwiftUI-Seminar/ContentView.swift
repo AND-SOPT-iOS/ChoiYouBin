48de7d8d797bbe3ddcaba1ba4a55dce642b87ec4
ChoiYouBin <cub8533@naver.com>
2024-12-05 01:15:35 +0900

    week/7-[Feat] SwiftUI 프로젝트 세팅

---
+++ SOPT-SwiftUI-Seminar/SOPT-SwiftUI-Seminar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SOPT-SwiftUI-Seminar
+//
+//  Created by 최유빈 on 12/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SOPT-SwiftUI-Seminar/SOPT-SwiftUI-Seminar/SOPT_SwiftUI_SeminarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SOPT_SwiftUI_SeminarApp.swift
+//  SOPT-SwiftUI-Seminar
+//
+//  Created by 최유빈 on 12/5/24.
+//
+
+import SwiftUI
+
+@main
+struct SOPT_SwiftUI_SeminarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

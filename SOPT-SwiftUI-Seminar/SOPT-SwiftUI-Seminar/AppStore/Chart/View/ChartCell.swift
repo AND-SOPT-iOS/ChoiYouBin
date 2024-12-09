e9e13d0b390c7ec029675e1711f1358f2640ab93
ChoiYouBin <cub8533@naver.com>
2024-12-10 02:25:54 +0900

    week/7-[Chore] FinanceChart category 분기 처리 수정

--- SOPT-SwiftUI-Seminar/SOPT-SwiftUI-Seminar/AppStore/Chart/View/ChartCell.swift
+++ SOPT-SwiftUI-Seminar/SOPT-SwiftUI-Seminar/AppStore/Chart/View/ChartCell.swift
@@ -36,7 +36,7 @@ struct ChartCell: View {
                         .frame(width: 55, height: 55)
                 }
             
-            if category == "essential" {
+            if category == "essential" || category == "finance" {
                 VStack(spacing: 3) {
                     Text(app.ranking.description)
                         .font(.system(size: 16, weight: .bold))

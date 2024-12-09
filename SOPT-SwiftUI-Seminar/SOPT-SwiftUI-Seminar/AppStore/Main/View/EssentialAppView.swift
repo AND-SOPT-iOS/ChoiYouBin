29816ba1e7a75fbf1dd13bb9c93d8d1adfa827f4
ChoiYouBin <cub8533@naver.com>
2024-12-10 00:48:53 +0900

    week/7-[Chore] NavigationView 범위 변경

--- SOPT-SwiftUI-Seminar/SOPT-SwiftUI-Seminar/AppStore/Main/View/EssentialAppView.swift
+++ SOPT-SwiftUI-Seminar/SOPT-SwiftUI-Seminar/AppStore/Main/View/EssentialAppView.swift
@@ -11,37 +11,39 @@ struct EssentialAppView: View {
     @StateObject private var chartViewModel = ChartViewModel()
     
     var body: some View {
-        VStack (spacing: 0){
-            Button {
-                
-            } label: {
-                HStack {
-                    Text("필수 금융 앱")
-                        .font(.system(size: 20, weight: .bold))
-                        .foregroundStyle(Color.black)
+        NavigationView {
+            VStack (spacing: 0){
+                Button {
                     
-                    Image(systemName: "chevron.right")
-                        .resizable()
-                        .frame(width: 10, height: 14)
-                        .foregroundStyle(Color(UIColor.darkGray))
+                } label: {
+                    HStack {
+                        Text("필수 금융 앱")
+                            .font(.system(size: 20, weight: .bold))
+                            .foregroundStyle(Color.black)
+                        
+                        Image(systemName: "chevron.right")
+                            .resizable()
+                            .frame(width: 10, height: 14)
+                            .foregroundStyle(Color(UIColor.darkGray))
+                        
+                        Spacer()
+                        
+                    }
+                }
+                .padding(.leading, 20)
+                
+                HStack {
+                    Text("App Store 에디터가 직접 골랐습니다")
+                        .font(.system(size: 12, weight: .semibold))
+                        .foregroundStyle(Color.gray)
                     
                     Spacer()
-                    
                 }
-            }
-            .padding(.leading, 20)
-            
-            HStack {
-                Text("App Store 에디터가 직접 골랐습니다")
-                    .font(.system(size: 12, weight: .semibold))
-                    .foregroundStyle(Color.gray)
+                .padding(.leading, 20)
                 
-                Spacer()
+                scrollView
+                    .scrollTargetBehavior(.paging)
             }
-            .padding(.leading, 20)
-            
-            scrollView
-                .scrollTargetBehavior(.paging)
         }
     }
     
@@ -49,29 +51,28 @@ struct EssentialAppView: View {
         ScrollView(.horizontal, showsIndicators: false) {
             LazyHGrid(rows: [GridItem(.flexible())], spacing: 0) {
                 ForEach(0..<3, id: \.self) { index in
-                    NavigationView {
-                        VStack {
-                            List(0..<3, id: \.self) { index in
-                                ZStack {
-                                    EssentialAppCell(chartViewModel: chartViewModel, index: index)
-                                    
-                                    NavigationLink(destination: TossView()) {
-                                        EmptyView()
-                                    }
-                                    .opacity(0)
+                    VStack {
+                        List(0..<3, id: \.self) { index in
+                            ZStack {
+                                EssentialAppCell(chartViewModel: chartViewModel, index: index)
+                                
+                                NavigationLink(destination: TossView()) {
+                                    EmptyView()
                                 }
+                                .opacity(0)
                             }
                         }
-                        .listStyle(.plain)
-                        .scrollDisabled(true)
                     }
-                    .frame(width: UIScreen.main.bounds.width)
+                    .listStyle(.plain)
+                    .scrollDisabled(true)
                 }
+                .frame(width: UIScreen.main.bounds.width)
             }
         }
         .frame(height: 230)
     }
 }
+
 
 #Preview {
     EssentialAppView()
--- SOPT-SwiftUI-Seminar/SOPT-SwiftUI-Seminar/AppStore/Main/View/MainRankingView.swift
+++ SOPT-SwiftUI-Seminar/SOPT-SwiftUI-Seminar/AppStore/Main/View/MainRankingView.swift
@@ -12,35 +12,37 @@ struct MainRankingView: View {
     let paid: String
     
     var body: some View {
-        VStack (spacing: 0){
-            NavigationLink(destination: FinanceChartView()
-                .navigationTitle("인기 차트")
-            ) {
-                HStack {
-                    if paid == "유료" {
-                        Text("유료 순위")
-                            .font(.system(size: 20, weight: .bold))
-                            .foregroundStyle(Color.black)
+        NavigationView {
+            VStack (spacing: 0){
+                NavigationLink(destination: FinanceChartView()
+                    .navigationTitle("인기 차트")
+                ) {
+                    HStack {
+                        if paid == "유료" {
+                            Text("유료 순위")
+                                .font(.system(size: 20, weight: .bold))
+                                .foregroundStyle(Color.black)
+                        }
+                        else {
+                            Text("무료 순위")
+                                .font(.system(size: 20, weight: .bold))
+                                .foregroundStyle(Color.black)
+                        }
+                        
+                        Image(systemName: "chevron.right")
+                            .resizable()
+                            .frame(width: 10, height: 14)
+                            .foregroundStyle(Color(UIColor.darkGray))
+                        
+                        Spacer()
+                        
                     }
-                    else {
-                        Text("무료 순위")
-                            .font(.system(size: 20, weight: .bold))
-                            .foregroundStyle(Color.black)
-                    }
-                    
-                    Image(systemName: "chevron.right")
-                        .resizable()
-                        .frame(width: 10, height: 14)
-                        .foregroundStyle(Color(UIColor.darkGray))
-                    
-                    Spacer()
-                    
                 }
+                .padding(.leading, 20)
+                
+                scrollView
+                    .scrollTargetBehavior(.paging)
             }
-            .padding(.leading, 20)
-            
-            scrollView
-                .scrollTargetBehavior(.paging)
         }
     }
     
@@ -48,24 +50,22 @@ struct MainRankingView: View {
         ScrollView(.horizontal, showsIndicators: false) {
             LazyHGrid(rows: [GridItem(.flexible())], spacing: 0) {
                 ForEach(0..<3, id: \.self) { index in
-                    NavigationView {
-                        VStack {
-                            List(0..<3, id: \.self) { index in
-                                ZStack {
-                                    ChartCell(chartViewModel: chartViewModel, index: index)
-                                    
-                                    NavigationLink(destination: TossView()) {
-                                        EmptyView()
-                                    }
-                                    .opacity(0)
+                    VStack {
+                        List(0..<3, id: \.self) { index in
+                            ZStack {
+                                ChartCell(chartViewModel: chartViewModel, index: index)
+                                
+                                NavigationLink(destination: TossView()) {
+                                    EmptyView()
                                 }
+                                .opacity(0)
                             }
                         }
-                        .listStyle(.plain)
-                        .scrollDisabled(true)
                     }
-                    .frame(width: UIScreen.main.bounds.width)
+                    .listStyle(.plain)
+                    .scrollDisabled(true)
                 }
+                .frame(width: UIScreen.main.bounds.width)
             }
         }
         .frame(height: 230)
